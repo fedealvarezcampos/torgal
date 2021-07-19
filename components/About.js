d269fe01ed0a79465da425bac883f97ad5c8dd48
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-07-19 17:37:36 +0200

    General styles, more to about compo

--- components/About.js
+++ components/About.js
@@ -1,4 +1,6 @@
 import styles from '../styles/About.module.css';
+import aboutImage from '../public/images/about.jpg';
+import Image from 'next/image';
 
 function About() {
     return (
@@ -9,6 +11,22 @@ function About() {
                     style={{ backgroundImage: `url(./images/about.jpg)` }}
                 ></div>
                 <div className={styles.aboutText}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui veritatis
+                    corporis? Natus qui ipsum, modi reprehenderit amet asperiores hic.
+                </div>
+            </div>
+            <div id="bar" className={styles.subContainer}>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src={aboutImage} // Route of the image file
+                        placeholder="blur"
+                        layout="responsive"
+                        quality={90}
+                        alt="about torgal"
+                    />
+                </div>
+                {/* <img className={styles.imgContainer} src="/images/about.jpg" alt="torgal foto" /> */}
+                <div className={styles.aboutText2}>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus qui veritatis
                     corporis? Natus qui ipsum, modi reprehenderit amet asperiores hic.
                 </div>
