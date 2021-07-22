e3c7faa3638d1f2b4bd0602eec43ceebc3faa893
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-07-22 20:26:10 +0200

    Responsive styles

--- components/Modal.js
+++ components/Modal.js
@@ -21,18 +21,18 @@ function Modal({ gigs, setModal }) {
                 <div className={styles.modalBG} onClick={() => setModal(false)} />
                 <div id="home" className={styles.contentContainer}>
                     <p className={styles.artistName}>{gigs.artist}</p>
-                    <YouTube className={styles.youtube} videoId={gigs.videoIntro} opts={opts} />
-                    {/* <iframe
-                            src={`https://www.youtube.com/embed/${gigs.videoIntro}`}
-                            title="YouTube video player"
-                            width="100%"
-                            height="100%"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            mute={1}
-                            autoplay={1}
-                            allowfullscreen
-                        ></iframe> */}
+                    {/* <YouTube className={styles.youtube} videoId={gigs.videoIntro} opts={opts} /> */}
+                    <iframe
+                        src={`https://www.youtube.com/embed/${gigs.videoIntro}?autoplay=1&mute=1&loop=1&playlist=${gigs.videoIntro}`}
+                        title="YouTube video player"
+                        width="100%"
+                        height="100%"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        mute="1"
+                        autoPlay="1"
+                        allowFullScreen
+                    />
                     <div
                         className={styles.image}
                         style={{ backgroundImage: `url(images/artists/${gigs.id}.jpg)` }}
