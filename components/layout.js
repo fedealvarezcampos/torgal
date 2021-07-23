2345f8a33c1cbbd801f603a65e5e3e1997d3d1ca
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-07-23 19:09:21 +0200

    General styles, favicon, ogimage, etc

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import styles from '../styles/layout.module.css';
-import ogimage from '../public/images/ogimage.jpg';
 
 export const siteTitle = 'Café & Pop Torgal';
 
@@ -10,7 +9,8 @@ export default function Layout({ children }) {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content="Café & Pop Torgal - Bar y sala de conciertos SON EG" />
-                <meta property="og:image" content={ogimage} />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="https://torgal.vercel.app/images/ogimage.jpg" />
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
