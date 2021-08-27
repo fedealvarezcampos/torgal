0c7e22086266669c2095be2fb094d5daa05ac189
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-08-27 13:36:09 +0200

    Changing header img, styles

--- components/Header.js
+++ components/Header.js
@@ -24,6 +24,7 @@ function Header() {
                         </li>
                     </ul>
                 </nav>
+                <script type="text/javascript"> </script>
             </header>
         </>
     );
