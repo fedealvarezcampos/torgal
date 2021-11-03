5fdeba8351e2a7617bdaa7d903182d0cac807a0a
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-11-03 12:59:20 +0100

    Next config changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const ContentSecurityPolicy = `
     default-src 'self';
-    script-src 'self' 'unsafe-inline' *.youtube.com https://maps.googleapis.com https://tumblr.us12.list-manage.com *.vercel.app *.vercel.com;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' *.youtube.com https://maps.googleapis.com https://tumblr.us12.list-manage.com *.vercel.app *.vercel.com;
     child-src *.youtube.com *.google.com;
     style-src 'self' 'unsafe-inline' *.googleapis.com;
     img-src * blob: data:;
@@ -13,6 +13,10 @@ const securityHeaders = [
     {
         key: 'Content-Security-Policy',
         value: ContentSecurityPolicy.replace(/\n/g, ''),
+    },
+    {
+        key: 'Access-Control-Allow-Origin',
+        value: '*.googleapis.com *.vercel.app *.vercel.com https://maps.googleapis.com https://tumblr.us12.list-manage.com',
     },
     {
         key: 'Referrer-Policy',
