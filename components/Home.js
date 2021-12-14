9a3d7db3ec972661412b8fa2ceffd80258699e2f
Fede Álvarez <fedealvarezcampos@gmail.com>
2021-12-15 00:38:25 +0100

    Adjusting image quality for header, minor style changes

--- components/Home.js
+++ components/Home.js
@@ -7,36 +7,36 @@ import back03 from '../public/images/back3.webp';
 import back04 from '../public/images/back4.webp';
 
 function Home() {
-    const bgArray = [back01, back02, back03, back04];
-    const [random, setRandom] = useState();
-    const randomBG = bgArray[random];
+	const bgArray = [back01, back02, back03, back04];
+	const [random, setRandom] = useState();
+	const randomBG = bgArray[random];
 
-    useEffect(() => {
-        setRandom(Math.floor(Math.random() * bgArray?.length));
-    }, [setRandom]);
+	useEffect(() => {
+		setRandom(Math.floor(Math.random() * bgArray?.length));
+	}, [setRandom]);
 
-    return (
-        <>
-            <div id="home" className={`${styles.container}`}>
-                <div className={styles.backImage}>
-                    {randomBG && (
-                        <Image
-                            src={randomBG}
-                            alt="header image"
-                            quality="90"
-                            layout="responsive"
-                            priority="true"
-                            placeholder="blur"
-                        />
-                    )}
-                </div>
-                <div className={`${styles.titleContainer} fadeInLeft`}>
-                    <img src="./images/stairs.png" width="455" height="456" alt="Torgal logo" />
-                    <img src="./images/LOGO.svg" width="807" height="296" alt="Torgal logo" />
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div id="home" className={`${styles.container}`}>
+				<div className={styles.backImage}>
+					{randomBG && (
+						<Image
+							src={randomBG}
+							alt="header image"
+							quality="85"
+							layout="responsive"
+							priority
+							placeholder="blur"
+						/>
+					)}
+				</div>
+				<div className={`${styles.titleContainer} fadeInLeft`}>
+					<img src="./images/stairs.png" width="455" height="456" alt="Torgal logo" />
+					<img src="./images/LOGO.svg" width="807" height="296" alt="Torgal logo" />
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Home;
