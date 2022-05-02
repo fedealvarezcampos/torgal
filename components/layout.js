e4d55b56cc3122c98d6ecaa869e336beb3e064e3
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-05-02 17:28:28 +0200

    Change meta description

--- components/layout.js
+++ components/layout.js
@@ -4,18 +4,20 @@ import styles from '../styles/layout.module.css';
 export const siteTitle = 'Café & Pop Torgal';
 
 export default function Layout({ children }) {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content="Café & Pop Torgal - Bar y sala de conciertos SON EG" />
-                <meta property="og:type" content="website" />
-                <meta property="og:image" content="https://torgal.vercel.app/images/ogimage.jpg" />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-            </Head>
-
-            {children}
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+				<meta
+					name="description"
+					content="Bar y sala de conciertos SON Estrella Galicia en Ourense, Galicia"
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="https://torgal.vercel.app/images/ogimage.jpg" />
+				<meta name="og:title" content={siteTitle} />
+				<meta name="twitter:card" content="summary_large_image" />
+			</Head>
+			{children}
+		</div>
+	);
 }
