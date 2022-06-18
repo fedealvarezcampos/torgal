164ad88d86328057eddec9193cccf7c713f1b0a1
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-06-18 11:06:29 +0200

    No gigs state, styles

--- components/sections/landing/ConcertList.js
+++ components/sections/landing/ConcertList.js
@@ -3,7 +3,7 @@ import { useClosingKey } from '../../../helpers/useClosingKey';
 import { motion as m, AnimatePresence } from 'framer-motion';
 import DateConverter from '../../basic/date';
 import ArtistInfo from './ArtistInfo';
-import styles from '../../../styles/Concerts.module.css';
+import s from '../../../styles/Concerts.module.css';
 
 function ConcertList({ gigs }) {
 	const [modal, setModal] = useState(false);
@@ -30,44 +30,51 @@ function ConcertList({ gigs }) {
 			</AnimatePresence>
 			<div
 				id="conciertos"
-				className={styles.container}
+				className={s.container}
 				style={{ backgroundImage: `url(./images/wallpaperFeather.webp)` }}
 			>
-				<ul className={styles.gigList}>
+				<ul className={s.gigList}>
 					{gigs.map(gigs => (
 						<m.li
 							initial={{ opacity: 0 }}
 							animate={{ opacity: 1 }}
-							className={`${styles.gigItem} ${gigs.gigDate < dateNow && styles.hidden}`}
+							className={`${s.gigItem} ${gigs.gigDate < dateNow && s.hidden}`}
 							key={gigs.id}
 						>
 							<DateConverter dateString={gigs.gigDate} />
-							<span className={styles.gigArtist}>{gigs.artist}</span>
+							<span className={s.gigArtist}>{gigs.artist}</span>
 							{gigs.bio && (
 								<button className="button" onClick={() => handleModal(gigs)}>
 									+ INFO
 								</button>
 							)}
-							<span className={styles.gigPrice}>{gigs.gigPrice}€</span>
-							<div className={styles.gigButtonCol}>
+							<span className={s.gigPrice}>{gigs.gigPrice}€</span>
+							<div className={s.gigButtonCol}>
 								{gigs.gigLink !== 'soldout' ? (
 									<a href={gigs.gigLink} target="_blank" rel="noreferrer">
 										<button
-											className={
-												gigs.soldout ? `button ${styles.gigSoldOut}` : 'button'
-											}
+											className={gigs.soldout ? `button ${s.gigSoldOut}` : 'button'}
 										>
 											{gigs.soldout ? <span>SOLD OUT</span> : 'ENTRADAS'}
 										</button>
 									</a>
 								) : (
-									<button className={styles.gigSoldOut}>
+									<button className={s.gigSoldOut}>
 										<span>SOLD OUT</span>
 									</button>
 								)}
 							</div>
 						</m.li>
 					))}
+					{gigs.length < 1 && (
+						<m.li
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							className={`${s.gigItem} ${s.noGigs}`}
+						>
+							¡Sin fechas de momento!
+						</m.li>
+					)}
 				</ul>
 			</div>
 		</>
